63371c098b4c9bad0ca827b2f83ca5ad32f305b0
Facundo Huergo <facuhuergo@hotmail.com>
2023-07-07 12:09:24 -0300

    Pelota y generador de mapa

---
+++ Assets/BallScript.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BallScript : MonoBehaviour
+{
+    [SerializeField] private float radio = 0.5f;
+    [SerializeField] private float scale = 1f;
+    [SerializeField] private Vector3 speed;
+    // [SerializeField] private PaletaScript paletaDerecha;
+    //Paletas
+    private float paredHorizontal = 5f;
+    private float paredVertical = 9.5f;
+    private Vector2 _wallLocation;
+    private float paletaLocation = 8.5f;
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        WallDetectionX(transform.position.x,_wallLocation.x);
+        WallDetectionY(transform.position.y,_wallLocation.y);
+        Move();
+        #region Deprecado
+
+        // if (transform.position.x >= paletaLocation - radio)
+        // {
+        //     float paletaD = paletaDerecha.transform.position.y; //Saco posicion paleta derecha en Y
+        //     //Si entro dentro del rango de la Paleta Derecha (teniendo encuenta su posicion y la altura de ella)
+        //     if (transform.position.y <= paletaD + paletaDerecha.altura && transform.position.y >= paletaD - paletaDerecha.altura)
+        //     {
+        //         velocidad.x = -Mathf.Abs(velocidad.x);  //Va en el sentido contrario
+        //     }
+        // } 
+        
+        // if(transform.position.x >= paredVertical - radio) //Si toque la pared Vertical Derecha, hize punto con la Izquierda
+        // {
+        //     OnIzquierdaPoints?.Invoke();
+        //     pointsSound.Play();
+        //     ResetPosition();
+        // }
+        //
+        //
+        // if(transform.position.x <= -paredVertical + radio)         //Si toque la Pared Vertical Izquierda, hice punto con Derecha
+        // {
+        //     OnDerechaPoints?.Invoke();
+        //     pointsSound.Play();
+        //     ResetPosition();
+        // }
+        //
+        // if (canMove) //MOVIMIENTO
+        // {
+        //     //La gravedad es una aceleracion, no una fuerza 
+        //     //La gravedad me modifica a la velocidad, la velocidad me modifica a la posicion
+        //     //gravedad -> velocidad -> posicion
+        //
+        //     //Formula de velocidad instanea: v = v + a * time
+        //     velocidad += gravedad * Time.deltaTime; 
+        //
+        //     //Formula MRUV: Xf = Xi + Vi * time + 1/2 a * t^2 
+        //     transform.position += velocidad * Time.deltaTime + 0.5f * gravedad * Time.deltaTime * Time.deltaTime;
+        // }
+
+        #endregion
+
+    }
+
+    public void AssignProperties(Vector2 wallLocation)
+    {
+        _wallLocation = wallLocation;
+    }
+    void WallDetectionX(float position, float wallLocationPosition)
+    {
+        //REBOTE EN X: Calculo la posicion en y de la pelota vs. la posicion de la pared horizontal menos/más el radio de la pelota. 
+        if (position >= wallLocationPosition - radio)
+            speed.x = -Mathf.Abs(speed.x); //Invierto la velocidad, haciendo que vaya en el sentido contrario
+
+        if (position <= -wallLocationPosition + radio)
+            speed.x = Mathf.Abs(speed.x);      
+
+    }
+
+    void WallDetectionY(float position, float wallLocationPosition)
+    {        //REBOTE EN X: Calculo la posicion en y de la pelota vs. la posicion de la pared horizontal menos/más el radio de la pelota. 
+        if (position >= wallLocationPosition - radio)
+            speed.y = -Mathf.Abs(speed.y); //Invierto la velocidad, haciendo que vaya en el sentido contrario
+
+        if (position <= -wallLocationPosition + radio)
+            speed.y = Mathf.Abs(speed.y);
+        
+    }
+    void ResetPosition() //Reseteo posicion de la pelota e inicio el timer para poder moverme
+    {
+        transform.position = Vector3.zero; //Vuelvo al lugar inicial
+    }
+
+    void Move()
+    {
+        transform.position += speed * Time.deltaTime;
+    }
+    
+}
--- Assets/MapCreation.cs
+++ Assets/MapCreation.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,22 @@ using UnityEngine;
 public class MapCreation : MonoBehaviour
 {
     [SerializeField]private Vector2 stageSize;
+    public Vector2 StageSize => stageSize;
     [SerializeField]private GameObject wallPrefab;
     [SerializeField]private List<GameObject> walls;
+    [SerializeField] private BallScript ball;
+    #region Editor
+
 #if UNITY_EDITOR
     [ContextMenu("BakeStage")]
     private void BakeStage()
     {
         var stageScaleX = new Vector3(stageSize.x, 1, 1);
         var stageScaleY = new Vector3(1, stageSize.y, 1);
-        InstantiateWall(new Vector3(0,0),stageScaleX);
-        InstantiateWall(new Vector3(0,stageSize.y),stageScaleX );
-        InstantiateWall(new Vector3(stageSize.x/2, stageSize.y/2), stageScaleY);
-        InstantiateWall(new Vector3(-stageSize.x/2,stageSize.y/2),stageScaleY);
+        InstantiateWall(new Vector3(0,stageSize.y/2),stageScaleX);
+        InstantiateWall(new Vector3(0,-stageSize.y/2),stageScaleX );
+        InstantiateWall(new Vector3(stageSize.x/2, 0), stageScaleY);
+        InstantiateWall(new Vector3(-stageSize.x/2,0),stageScaleY);
     }
     [ContextMenu("ClearStage")]
     private void ClearScenenary()
@@ -30,6 +35,14 @@ public class MapCreation : MonoBehaviour
         walls.Clear();
     }
 #endif
+
+    #endregion
+
+    private void Start()
+    {
+        ball.AssignProperties(stageSize / 2);
+    }
+
     private void InstantiateWall(Vector3 pos, Vector3 scale)
     {
         var wall =   Instantiate(wallPrefab,pos,Quaternion.identity);
