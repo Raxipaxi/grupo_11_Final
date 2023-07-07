ace39b8767a1c1564c4e0607ad9f2a423656a42c
Facundo Huergo <facuhuergo@hotmail.com>
2023-07-07 15:39:09 -0300

    Brick Prefab

--- Assets/Brick.cs
+++ Assets/Brick.cs
@@ -1,9 +1,16 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Utilities
 {
     public class Brick : MonoBehaviour
     {
+        [SerializeField] private Color[] _colors;
 
+        private void Start()
+        {
+            GetComponent<MeshRenderer>().material.color = _colors[Random.Range(0,_colors.Length)];
+        }
     }
 }
