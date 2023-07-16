cc7694b89fd8b9347cbfef7958820c2bf838a37c
Facundo Huergo <facuhuergo@hotmail.com>
2023-07-16 00:53:28 -0300

    bricks en lista y prototipo en physics manager

--- Assets/Scripts/MapCreation.cs
+++ Assets/Scripts/MapCreation.cs
@@ -11,11 +11,9 @@ public class MapCreation : MonoBehaviour
     [SerializeField] private GameObject[] wallsY;
     [SerializeField] private Vector2 brickSize;
     public Vector2 BrickSize => brickSize;
-    private Dictionary<Vector2, Bricks> _bricksMap = new Dictionary<Vector2, Bricks>();
-    public Dictionary<Vector2, Bricks> BricksMap => _bricksMap;
+    [field: SerializeField] public List<Bricks> BricksList;
     [SerializeField] private BallScript ball;
     [SerializeField] private Slider _slider;
-    [SerializeField] private Bricks[] bricks;
     [SerializeField]private PhysicsManager _manager;
 
     private void Awake()
@@ -25,11 +23,6 @@ public class MapCreation : MonoBehaviour
     }
     private void Start()
     {
-        for (int i = 0; i < bricks.Length; i++)
-        {
-            if(_bricksMap.ContainsKey(bricks[i].transform.position))
-                _bricksMap.Add(bricks[i].transform.position, bricks[i]);
-        }
         _slider.AssignProperties(stageSize);
         ball.AssignProperties(stageSize,brickSize.y,_slider,_manager);
 
--- Assets/Scripts/PhysicsManager.cs
+++ Assets/Scripts/PhysicsManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 
@@ -8,11 +9,12 @@ namespace Utilities
     {
         private Slider _slider;
         private MapCreation _map;
-
-         public void Initialize (Slider slider, MapCreation mapCreator)
+        private List<Bricks> _bricksList;
+         public void Initialize (Slider slider, MapCreation mapCreator, List<Bricks> bricksList)
         {
             _slider = slider;
             _map = mapCreator;
+            _bricksList = bricksList;
         }
 
         public Vector2 SliderCollision(BallScript ball, float radio)
@@ -31,12 +33,6 @@ namespace Utilities
                 print("Centro");
        
                 return new Vector2(ball.Dir.x, Math.Abs(ball.Dir.y));
-                 
-                    
-                
-
-                
-
             }
             // if (ball.transform.position.x + radio <= _slider.transform.position.x - _slider.Size.x) // Si toco la mitad del lado izquierdo (negativo) me voy para x en el lado izquierdo
             // {
@@ -50,25 +46,45 @@ namespace Utilities
 
             return ball.Dir;
         }
-        public Vector2 BrickCollisionCheck(BallScript ball )
+        public Vector2 BrickCollisionCheck(BallScript ball, float radio)
         {
-            if (_map.BricksMap.ContainsKey(ball.Pos)) // Estoy en el centro
-            {
-                return new Vector2(ball.Dir.x, Math.Abs(-ball.Dir.y));
+            // for (int i = 0; i < _bricksList.Count; i++)
+            // {
+            //     Bricks currBrick = _bricksList[i];
+            //     if (ball.Pos.x - radio <= currBrick.PosX + _slider.Size &&
+            //         ball.Pos.x + radio >= currBrick.PosX - _slider.Size)
+            //     {
+            //         float ballPosX = ball.Pos.x;
+            //         if (ballPosX < currBrick.PosX - currBrick.Center ||
+            //             ballPosX > currBrick.PosX + currBrick.Center)
+            //         {
+            //             print("cotadito");
+            //             return new Vector2(ball.Dir.x - 0.25f, Math.Abs(ball.Dir.y));
+            //         }
+            //
+            //         print("Centro");
+            //
+            //         return new Vector2(ball.Dir.x, Math.Abs(ball.Dir.y));
+            //     }
+            // }
 
-            } 
-            if(_map.BricksMap.ContainsKey((Vector2) ball.Pos + _map.BrickSize / 2)) // DER
-            {
-                return new Vector2(ball.Dir.x - 0.25f, Math.Abs(-ball.Dir.y));
-
-            }
-
-            if (_map.BricksMap.ContainsKey((Vector2) ball.Pos + -_map.BrickSize / 2)) // IZQ
-            {
-                return new Vector2(ball.Dir.x - 0.25f, Math.Abs(-ball.Dir.y));
-
-
-            }
+            // if (_map.BricksMap.ContainsKey(ball.Pos)) // Estoy en el centro
+            // {
+            //     return new Vector2(ball.Dir.x, Math.Abs(-ball.Dir.y));
+            //
+            // } 
+            // if(_map.BricksMap.ContainsKey((Vector2) ball.Pos + _map.BrickSize / 2)) // DER
+            // {
+            //     return new Vector2(ball.Dir.x - 0.25f, Math.Abs(-ball.Dir.y));
+            //
+            // }
+            //
+            // if (_map.BricksMap.ContainsKey((Vector2) ball.Pos + -_map.BrickSize / 2)) // IZQ
+            // {
+            //     return new Vector2(ball.Dir.x - 0.25f, Math.Abs(-ball.Dir.y));
+            //
+            //
+            // }
 
             return ball.Dir;
         }
